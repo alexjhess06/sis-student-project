9fc30738d88c6e8a3c2519eff7b5084f83e52843
Dominic Moser <llamapagama@gmail.com>
2024-03-04 09:53:23 -0700

    Added some skeleton 'class' code for a student object.
    Added some skeleton 'class' code for a student object.
    
    Created Main and Student classes with basic attributes and constructor.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # sis-student-project
 A project made and maintained by myself and three classmates, listed in the contributions
-
-## HELLO WORLD ##
---
+++ project/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String a;
+    String b;
+    String c;
+    public Student(String a, String b, String c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
